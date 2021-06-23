7de413db76bb15d63a9aed42d721ac82b7cce8dc
SNEHA K S <83630092+Sneha-coder08@users.noreply.github.com>
2021-06-23 19:54:01 +0530

    Create DAY 11 MYMODULE.py

---
+++ DAY 11 MYMODULE.py
@@ -0,0 +1,6 @@
+change the value in list"""
+list1=['Sanil','Kavitha','Sneha','Naveen']
+def mymodule(list1):
+    print("Content from user defined module !!")
+    for i in list1:
+        return "Hello ",i
